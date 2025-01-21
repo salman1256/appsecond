ef181ec1393c08a4c1c850ff23f3f3e1ab3a3f4a
MOHD SALMAN <salman1256@yahoo.com>
2025-01-21 09:19:38 +0530

    After Adding Stack

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Stack<int> myStack=new Stack<int>();
+myStack.Push(1);
+myStack.Push(2);
+myStack.Push(3);
+myStack.Push(4);
+myStack.Push(5);
+System.Console.WriteLine("Number of items in Stack "+myStack.Count);
+foreach(var item in myStack)
+{
+    System.Console.WriteLine(item);
+}
+
+
+
