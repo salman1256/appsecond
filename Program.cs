6233c88c8fa5d4e74f36c1cb99cc373791e18052
MOHD SALMAN <salman1256@yahoo.com>
2025-01-21 10:59:29 +0530

    Again

--- Program.cs
+++ Program.cs
@@ -1,11 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Stack Discussion ");
 Stack<int> myStack=new Stack<int>();
 myStack.Push(1);
 myStack.Push(2);
 myStack.Push(3);
 myStack.Push(4);
 myStack.Push(5);
+myStack.Push(6);
 System.Console.WriteLine("Number of items in Stack "+myStack.Count);
 foreach(var item in myStack)
 {
